1bc7449d414c8bea63ef334f44b3464df36f5687
Danil <danya.trofimov@gmail.com>
2022-04-21 09:36:38 +0300

    switch-case and controller validation

--- src/com/kpi/Main.java
+++ src/com/kpi/Main.java
@@ -1,7 +1,5 @@
 package com.kpi;
 
-import com.kpi.exceptions.EmptyResultException;
-
 public class Main {
 
     public static void main(String[] args) {
--- src/com/kpi/SubscriberController.java
+++ src/com/kpi/SubscriberController.java
@@ -1,33 +1,42 @@
 package com.kpi;
 
-import com.kpi.exceptions.EmptyResultException;
+import com.kpi.exceptions.WrongFunctionException;
+import com.kpi.exceptions.WrongInputNumberException;
 
 public class SubscriberController {
     private SubscriberModel model;
     private SubscriberView view;
 
-    public SubscriberController(SubscriberModel model, SubscriberView view){
+    public SubscriberController(SubscriberModel model, SubscriberView view) {
         this.model = model;
         this.view = view;
     }
 
     public void start() {
-        do {
-            int func = view.chooseFunction();
-            if(func == 0){
-                break;
-            }
-            else if(func == 1){
-                int minutes = view.countOfMinutes();
-                if(minutes == -1){
-                    view.getPrinter().printTryAgain();
-                    continue;
+        boolean work = true;
+        try {
+            do {
+                int func = view.chooseFunction();
+                Validator.checkFunctionType(func);
+                switch (func) {
+                    case 0: {
+                        work = false;
+                        break;
+                    }
+                    case 1: {
+                        int minutes = view.countOfMinutes();
+                        Validator.validatePositiveNumbersInput(Integer.toString(func));
+                        view.printArray(model.intracityMoreThen(minutes));
+                        break;
+                    }
+                    case 2: {
+                        view.printArray(model.usedIntercityCommunication());
+                        break;
+                    }
                 }
-                view.printArray(model.intracityMoreThen(minutes));
-            }
-            else if(func == 2){
-                view.printArray(model.usedIntercityCommunication());
-            }
-        }while (true);
+            } while (work);
+        } catch (WrongInputNumberException | WrongFunctionException e) {
+            view.getPrinter().print(e.getMessage());
+        }
     }
 }
